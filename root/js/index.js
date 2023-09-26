f7113131125a769514cd3df4fea197d7347fa9cf
ph2190046 <ph2190046@transcosmos-ph.local>
2023-09-26 16:17:20 +0800

    update js

--- root/js/index.js
+++ root/js/index.js
@@ -3,14 +3,23 @@ const thank = document.querySelector(".thankBox");
 const submit = document.querySelector(".submitBtn");
 const rating = document.getElementById("rating");
 const val = document.querySelectorAll(".value");
+var count = 0;
 
 submit.addEventListener ( "click", () => {
 	thank.classList.remove("hide");
 	rate.style.display = "none";
 });
 
+if (count == 0) {
+	submit.setAttribute('disabled', 'disabled');
+}
+
 val.forEach((num) => {
 	num.addEventListener ("click", () => {
 		rating.innerHTML = num.innerHTML;
+		count++;
+		submit.removeAttribute('disabled');
 	});
 });
+
+
